1d662d5ee5f6c09a2ed5b968ab59d9b9d11a15ff
Diego Quispe <drqc0491@gmail.com>
2025-02-14 00:23:15 +0000

    updates

--- README.md
+++ README.md
@@ -22,18 +22,22 @@ export DYNATRACE_ENVIRONMENT_ID=""
 
 To quickly run the container you can use the following command 
 
-```
+```(bash)
 echo $DYNATRACE_TOKEN | docker login $DYNATRACE_ENVIRONMENT_URL -u $DYNATRACE_ENVIRONMENT_ID --password-stdin
 sed -i "s|DTenvironmentURL|$DYNATRACE_ENVIRONMENT_URL|g" ./Dockerfile
 docker build -t dynatrace-nodejs-demo:latest .
 docker run --name production-dt-nodejs-demo -p 3000:3000 dynatrace-nodejs-demo:latest
+docker run --name load-generator -p 8089:8089 -e LOCUST_LOAD_TEST_NAME=demoLoad -v $PWD/load-test:/mnt/locust locustio/locust -f /mnt/locust/locustfile.py -H http://localhost:3000
 ```
+
+
+-v ${pwd}:/usr/
+
 
 Once you finish to remove the container and image run the following
 
-```
-docker stop production-dt-nodejs-demo
-docker rm production-dt-nodejs-demo
+```(bash)
+docker stop production-dt-nodejs-demo && docker rm production-dt-nodejs-demo
 docker image rm dynatrace-nodejs-demo:latest
-
+docker stop load-generator && docker rm load-generator
 ```
--- index.ts
+++ index.ts
@@ -11,30 +11,24 @@ const port = process.env.PORT || 3000;
 type User = {
   name: string;
   lastName: string;
-  username: string;
+  email: string;
 }
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.get("/create-user", async (req: Request, res: Response) => {
-  const user: User = {
-    name: req.query.name as string,
-    lastName: req.query.lastName as string,
-    username: req.query.username as string,
-  };
-
+app.post("/create-user", async (req: Request, res: Response) => {
+  const user: User = req.body;
   try {
-   // const response = await axios.post("https://jsonplaceholder.typicode.com/users", user);
-    const response = await fetch("http://example.com/api/endpoint", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
-      // . . .
-    })
-    res.status(201).send(response.json());
+  //  // const response = await axios.post("https://jsonplaceholder.typicode.com/users", user);
+  //   const response = await fetch("http://example.com/api/endpoint", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(user),
+  //   });
+  //  res.status(201).send(response.json());
   } catch (error) {
     res.status(500).send("Error creating user");
   }
